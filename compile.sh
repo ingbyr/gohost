74f484d6a4ebf84ad9dde47f1b31e1af292dd609
ingbyr <ingbyr@qq.com>
2021-09-27 11:27:39 +0800

    config goreleaser

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o gohost-windows-32bit.exe .
-#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o gohost-windows-64bit.exe .
-
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o gohost-linux-32bit .
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gohost-linux-64bit .
-
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o gohost-darwin .
