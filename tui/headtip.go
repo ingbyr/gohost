1423c9526f10b7c5c4d82c4a9cf0405566e03e7d
ingbyr <ingbyr@qq.com>
2022-09-14 17:31:00 +0800

    add headtip

---
+++ tui/headtip.go
@@ -0,0 +1,23 @@
+package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type HeadTip struct {
+}
+
+func (h *HeadTip) Init() tea.Cmd {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HeadTip) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HeadTip) View() string {
+	//TODO implement me
+	panic("implement me")
+}
